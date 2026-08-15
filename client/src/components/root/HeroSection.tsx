d6f048b09fa5bdc3a3bacfa59bfeececc5cd8b79
ssb-shree <bavachikarshree1666@gmail.com>
2026-08-15 15:18:02 +0530

    make hero toast a seprate component and fetch data from server

--- client/src/components/root/HeroSection.tsx
+++ client/src/components/root/HeroSection.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+
 
 import { titleFont } from "@/app/layout";
+import HeroToast from "./HeroToast";
+import axiosInstance from "@/services/axios";
+import { EventType } from '@/app/events/create/page';
 
 const HeroSection = () => {
+
   return (
     <section className="relative flex h-screen w-screen flex-col items-center justify-center">
       <h1
@@ -16,7 +20,7 @@ const HeroSection = () => {
         <span className="text-nowrap">Math &</span>
         <span className="text-nowrap">Science Club</span>
       </h1>
-
+  
       <div className="absolute bottom-0 p-10 left-0 h-1/2 w-full flex justify-between items-end">
         <p className="w-full md:w-[40vw] cursor-target flex flex-col">
           <span className="uppercase text-lg"># Curiosity meets discovery.</span>
@@ -25,24 +29,11 @@ const HeroSection = () => {
             projects, and hands-on exploration.
           </span>
         </p>
-
-        <div className="hidden cursor-target cursor-pointer md:flex flex-row justify-around items-center border-2 border-black rounded-sm w-[30%] h-1/2 p-x">
-          <img
-            src={"https://th.bing.com/th/id/OIP.sUzI9MNZRL8yQWHYt05PlQHaEK?w=320&h=180&c=7&r=0&o=7&pid=1.7&rm=3"}
-            className="h-full w-[35%] object-cover p-1"
-          />
-          <div className="flex flex-col justify-around items-start">
-            <h3 className="flex flex-row gap-1 justify-start items-center">
-              <div className="size-2 bg-red-400" />
-              <span className="text-xs p-2">Upcoming</span>
-            </h3>
-            <h2 className="cursor-target">Event Event Name Full </h2>
-            <span className="text-xs">08 June, 2026</span>
-          </div>
-        </div>
+  
+        <HeroToast />
       </div>
     </section>
   );
-};
+}
 
 export default HeroSection;
---
+++ client/src/components/root/HeroToast.tsx
@@ -0,0 +1,29 @@
+"use server";
+import { EventType } from "@/app/events/create/page";
+import axiosInstance from "@/services/axios";
+
+const HeroToast = async () => {
+  try {
+    const {
+      data: { events },
+    }: { data: { events: EventType } } = await axiosInstance.get("/events/latest", { withCredentials: true });
+
+    return (
+      <div className="hidden cursor-target cursor-pointer md:flex flex-row justify-around items-center border-2 border-black rounded-sm w-[30%] h-1/2 p-x">
+        <img src={events.banner} className="h-full w-[35%] object-cover p-1" />
+        <div className="flex flex-col justify-around items-start">
+          <h3 className="flex flex-row gap-1 justify-start items-center">
+            <div className="size-2 bg-red-400" />
+            <span className="text-xs p-2">Upcoming</span>
+          </h3>
+          <h2 className="cursor-target">{events.title}</h2>
+          <span className="text-xs">{new Date(events.createdAt!).toDateString()}</span>
+        </div>
+      </div>
+    );
+  } catch (error) {
+    return null;
+  }
+};
+
+export default HeroToast;
