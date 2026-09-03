e6b7bf7577282daad1523e1cb296038e5d3dd276
ssb-shree <bavachikarshree1666@gmail.com>
2026-09-03 22:52:17 +0530

    replace register text of button with check out

--- client/src/app/events/page.tsx
+++ client/src/app/events/page.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { EventType } from '@/app/events/create/page';
 import { useRouter } from "next/navigation";
+import LoadingPage from "../loading";
 
 const EventsPage = () => {
   const [eventData, setEventData] = useState<EventType[]>([]);
@@ -24,11 +25,15 @@ const EventsPage = () => {
     fetchEvents();
   }, []);
 
+  if(!eventData){
+    return <LoadingPage/>
+  }
+
   return (
     <section className=" min-h-screen mt-20">
 
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 pt-12">
-        {eventData.map((event, i) => (
+        {eventData && eventData.map((event, i) => (
           <div key={i} className="aspect-[3/4] border border-white/10 flex items-center justify-center flex-col">
             <Link className="h-full w-full" href={`/events/${event.slug}`}>
               <img src={event.banner} alt={`${event.title}`} className="h-full w-full object-cover cursor-target" />
@@ -37,7 +42,7 @@ const EventsPage = () => {
               onClick={() => router.push(`/events/${event.slug}`)}
               className="text-center w-full rounded-none bg-black text-white pb-2 cursor-target"
             >
-              Register
+              Check Out
             </button>
           </div>
         ))}
