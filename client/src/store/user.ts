0a2624cc5636d78ec50bcd3ef7dd2878460ace3f
ssb-shree <bavachikarshree1666@gmail.com>
2026-09-03 22:56:26 +0530

    add a function to fetch user details

--- client/src/store/user.ts
+++ client/src/store/user.ts
@@ -1,3 +1,5 @@
+import { toasty } from "@/components/ToastProvider";
+import axiosInstance from "@/services/axios";
 import { create } from "zustand";
 
 export type UserType = {
@@ -19,11 +21,29 @@ type userStore = {
   setUser: (data: UserType | null) => void;
   isAuth: boolean;
   setAuth: (data: boolean) => void;
+  getUser : ()=> Promise<void>;
 };
 
-export const useUserStore = create<userStore>()((set) => ({
+export const useUserStore = create<userStore>()((set, get) => ({
   user: null,
   setUser: (data: UserType | null) => set(() => ({ user: data })),
   isAuth: false,
   setAuth: (data: boolean) => set(() => ({ isAuth: data })),
+  getUser : async () => {
+    const {setAuth, setUser} = get()
+      try {
+        const { data } = await axiosInstance.get("/auth/check", { withCredentials: true });
+        setAuth(true);
+        setUser(data.user);
+      } catch (error: any) {
+        if (error.response.data.message === "jwt malformed") {
+          toasty("cookie expired, login again");
+        }
+  
+        if (error.response.data.message === "unauthorized no token provided") {
+          toasty("login into your accoun to see profile");
+        }
+        setAuth(false);
+      }
+    }
 }));
