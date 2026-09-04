c3d7b761ed95663348af56a8a03ff87d8d6ce2ca
ssb-shree <bavachikarshree1666@gmail.com>
2026-09-04 23:12:50 +0530

    add IT to allowed department list

--- client/src/components/events/UpdateEventDetails.tsx
+++ client/src/components/events/UpdateEventDetails.tsx
@@ -47,7 +47,7 @@ const UpdateEventDetails = ({ event }: { event: EventType }) => {
   const router = useRouter();
 
   const allowedYears = ["FE", "SE", "TE", "BE"];
-  const allowedDepartments = ["COMP", "DS", "AIML", "CIVIL", "MECH"];
+  const allowedDepartments = ["COMP", "DS", "AIML", "CIVIL", "MECH", "IT"];
   const allowedDivisions = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
   const handleAllowedYears = (value: string, checked: boolean) => {
