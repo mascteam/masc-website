9dfc45baab2b887a14f71b4565e31917053ea839
ssb-shree <bavachikarshree1666@gmail.com>
2026-09-04 14:40:45 +0530

    Add page to update existing blogs

--- client/src/app/blogs/[slug]/(admin)/update/page.tsx
+++ client/src/app/blogs/[slug]/(admin)/update/page.tsx
@@ -1,9 +1,40 @@
 "use client";
 
-import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor";
+import NotFound from "@/app/not-found";
+import BlogForm, { BlogData } from "@/components/blogs/BlogForm";
+import axiosInstance from "@/services/axios";
+
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const UpdateBlogPage = () => {
-  // return <SimpleEditor  />;
+  const [blog, setBlog] = useState<BlogData | null>(null);
+
+  const { slug } = useParams();
+
+  useEffect(() => {
+    const getBlogData = async () => {
+      try {
+        const { data } = await axiosInstance.get(`/blogs/${slug}`);
+        setBlog(data.blog);
+      } catch (error: any) {
+        console.error(error.message || error);
+        setBlog(null);
+      }
+    };
+
+    getBlogData();
+  }, []);
+
+  if (!blog) {
+    return <NotFound />;
+  }
+
+  return (
+    <main className="min-h-screen px-6 py-16 text-black">
+      <BlogForm mode="create" initialData={blog} />
+    </main>
+  );
 };
 
 export default UpdateBlogPage;
