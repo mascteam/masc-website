d7d18b67ce9f3bad98561ca403527b0fe00c1acf
ssb-shree <bavachikarshree1666@gmail.com>
2026-09-05 14:41:27 +0530

    diff bg component for mobile and pc screen

--- client/src/components/root/HeroSection.tsx
+++ client/src/components/root/HeroSection.tsx
@@ -26,7 +26,7 @@ const HeroSection = () => {
         spin={0.5}
         interaction="repel"
         density={0.6}
-        shardSize={0.35}
+        shardSize={1}
         stretch={0.95}
         turbulence={1}
         glow={1}
@@ -40,7 +40,39 @@ const HeroSection = () => {
         rippleIntensity={1}
         holdToGather
         paused={false}
-        className="h-full w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        className="h-full w-full hidden md:flex absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      />
+      <AeroShards
+        backgroundColor="#F1F5F9"
+        shardColor="#080114"
+        accentColor="#c41f1f"
+        placement="full"
+        flow="stream"
+        material="pearl"
+        detail="balanced"
+        effect="none"
+        scale={0.65}
+        spread={1}
+        depth={1}
+        speed={0.4}
+        spin={0.5}
+        interaction="repel"
+        density={0.6}
+        shardSize={1}
+        stretch={0.95}
+        turbulence={1}
+        glow={1}
+        edgeSoftness={1.6}
+        bloom={0.4}
+        grain={0.05}
+        chromaticAberration={0.0075}
+        transitionDuration={1}
+        interactionRadius={1.5}
+        interactionStrength={0.5}
+        rippleIntensity={1}
+        holdToGather
+        paused={false}
+        className="h-full w-full md:hidden absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
       />
       <section className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex h-screen w-screen items-center justify-center">
         <CardContainer className="h-full w-full">
--- client/src/components/root/HeroToast.tsx
+++ client/src/components/root/HeroToast.tsx
@@ -1,29 +1,35 @@
-"use server";
+"use client";
 import { EventType } from "@/app/events/create/page";
 import axiosInstance from "@/services/axios";
+import { useEffect, useState } from "react";
 
-const HeroToast = async () => {
-  try {
-    const {
-      data: { events },
-    }: { data: { events: EventType } } = await axiosInstance.get("/events/latest", { withCredentials: true });
+const HeroToast = () => {
+  const [event, setEvent] = useState<EventType | null>(null);
 
-    return (
+  useEffect(() => {
+    const fetchUpcomingEvent = async () => {
+      const { data } = await axiosInstance.get("/events/latest", { withCredentials: true });
+      setEvent(data.event);
+    };
+
+    fetchUpcomingEvent();
+  }, []);
+
+  return (
+    event && (
       <div className="hidden cursor-target cursor-pointer md:flex flex-row justify-around items-center border-2 border-black rounded-sm w-[30%] h-1/2 p-x">
-        <img src={events.banner} className="h-full w-[35%] object-cover p-1" />
+        <img src={event.banner} className="h-full w-[35%] object-cover p-1" />
         <div className="flex flex-col justify-around items-start">
           <h3 className="flex flex-row gap-1 justify-start items-center">
             <div className="size-2 bg-red-400" />
             <span className="text-xs p-2">Upcoming Event</span>
           </h3>
-          <h2 className="cursor-target">{events.title}</h2>
-          <span className="text-xs">{new Date(events.createdAt!).toDateString()}</span>
+          <h2 className="cursor-target">{event.title}</h2>
+          <span className="text-xs">{new Date(event.createdAt!).toDateString()}</span>
         </div>
       </div>
-    );
-  } catch (error) {
-    return null;
-  }
+    )
+  );
 };
 
 export default HeroToast;
