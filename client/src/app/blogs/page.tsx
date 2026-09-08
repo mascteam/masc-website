6e7213cb52a7638c71e0d45139efd159e643af11
ssb-shree <bavachikarshree1666@gmail.com>
2026-09-08 21:41:05 +0530

    convert to a csr page

--- client/src/app/blogs/page.tsx
+++ client/src/app/blogs/page.tsx
@@ -1,29 +1,49 @@
-"use server";
+"use client";
 
 import BlogPreview from "@/components/blogs/BlogPreview";
 import LoadingPage from "../loading";
 import axiosInstance from "@/services/axios";
 import { Blog } from "./[slug]/page";
+import { useEffect, useState } from "react";
+import { useLoadingStore } from "@/store/loading";
+import NotFound from "../not-found";
+import { toasty } from "@/components/ToastProvider";
 
-const BlogPage = async () => {
-  try {
-    const {
-      data: { blogs },
-    }: { data: { blogs: Blog[] } } = await axiosInstance.get("/blogs");
+const BlogPage = () => {
+  const [blogs, setBlogs] = useState<Blog[]>([]);
 
-    if (!blogs || blogs.length < 1) throw new Error("failed to fetch blogs");
+  const { loading, setLoading } = useLoadingStore();
 
-    return (
-      <main className="min-h-[80vh] flex flex-col justify-center items-center md:px-10 mt-10">
-        {blogs.map((blog) => (
-          <BlogPreview key={blog.slug} blogContent={blog} />
-        ))}
-      </main>
-    );
-  } catch (error : any) {
-    console.error(error.message || error)
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      setLoading(true);
+      try {
+        const {
+          data: { blogs },
+        }: { data: { blogs: Blog[] } } = await axiosInstance.get("/blogs");
+
+        setBlogs(blogs);
+      } catch (error: any) {
+        toasty(error.resonse.data.message);
+        return <NotFound />;
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchBlogs();
+  }, []);
+
+  if (loading) {
     return <LoadingPage />;
   }
+
+  return (
+    <main className="min-h-[80vh] flex flex-col justify-center items-center md:px-10 mt-10">
+      {blogs.map((blog) => (
+        <BlogPreview key={blog.slug} blogContent={blog} />
+      ))}
+    </main>
+  );
 };
 
 export default BlogPage;
