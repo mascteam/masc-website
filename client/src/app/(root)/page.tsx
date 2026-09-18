ad668bed9ed82658b9774ea1072df2cefc297845
ssb-shree <bavachikarshree1666@gmail.com>
2026-09-18 13:52:39 +0530

    hide team section and marqee

--- client/src/app/(root)/page.tsx
+++ client/src/app/(root)/page.tsx
@@ -4,13 +4,8 @@ import EventSection from "@/components/root/EventSection";
 import HeroSection from "@/components/root/HeroSection";
 import TeamSection from "@/components/root/TeamSection";
 import { Marquee } from "@/components/ui/marquee";
+import { poppins } from "@/lib/fonts";
 import { Metadata } from "next";
-import { Poppins } from "next/font/google";
-
-const poppins = Poppins({
-  weight: "700",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Home",
@@ -26,15 +21,15 @@ const Rootpage = () => {
     <>
       <HeroSection />
       <AboutSection />
-      <Marquee className=" border-t-2 border-black select-none">
+      {/* <Marquee className=" border-t-2 border-black select-none">
         <p
-          className={`font-mono text-[10vh] md:text-[15vh] font-bold text-black [-webkit-text-stroke:2px_white] uppercase ${poppins.className}`}
+          className={`font-mono text-[10vh] md:text-[15vh] font-bold text-black [-webkit-text-stroke:2px_black] uppercase`}
         >
-          <span className="hidden md:flex">Minds Behind masc</span>
-          <span className="md:hidden flex">Team masc</span>
+          <span className={`${poppins.className} hidden md:flex`}>Minds Behind masc</span>
+          <span className={`${poppins.className} md:hidden flex`}>Team masc</span>
         </p>
       </Marquee>
-      <TeamSection />
+      <TeamSection /> */}
       <EventSection />
       <BlogSection />
     </>
