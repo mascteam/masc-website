bd5fe87f850d9d6bb48bd54344df41392a3a446e
Shree Bavachikar <bavachikarshree1666@gmail.com>
2026-09-24 08:29:28 +0530

    add json LD for better seo

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -77,10 +77,27 @@ export const metadata: Metadata = {
   },
 };
 
+
+const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  name: "MASC",
+  alternateName: "Math & Applied Science Club",
+  url: "https://masc-apsit.vercel.app",
+  description:
+    "Math & Applied Science Club at APSIT, bringing students together through mathematics, science, technology, events, workshops, and exploration.",
+};
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={`${globalFont.className}`}>
       <body className="flex flex-col text-black bg-slate-100 overflow-x-hidden">
+<script
+  type="application/ld+json"
+  dangerouslySetInnerHTML={{
+    __html: JSON.stringify(jsonLd),
+  }}
+/>
         <LenisProvider>
           <Toaster position="bottom-right" reverseOrder={false} />
           <TargetCursor
