c9abc4aed05a51a161b70ab31760050064c037e0
ssb-shree <bavachikarshree1666@gmail.com>
2026-09-25 22:39:26 +0530

    update robotos.txt

--- client/src/app/robots.ts
+++ client/src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/admin/", "/auth/"],
+      disallow: ["/admin/", "/auth/profile/"],
     },
     sitemap: `${BASE_URL}/sitemap.xml`,
   };
